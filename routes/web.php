ba39bd5e68a5edbe9ace68120fa56ed3f816ad0d
kenmutesh <kenmutesh901@gmail.com>
2024-05-26 13:34:09 +0300

    load messages

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
     use App\Http\Controllers\HomeController;
+    use App\Http\Controllers\MessageController;
     use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -11,12 +12,14 @@ use Inertia\Inertia;
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', [HomeController::class, 'home'])->name('home');
 
-    Route::get('/user/{user}', function (){
+   Route::prefix('/')->controller(MessageController::class)->group(function (){
+       Route::get('user/{user}', 'byUser')->name('chat.user');
+       Route::get('user/{group}', 'byGroup')->name('chat.group');
+       Route::get('message/older/{message}', 'loadOlder')->name('message.loadOlder');
 
-    })->name('chat.user');
-    Route::get('/user/{group}', function (){
-
-    })->name('chat.group');
+       Route::post('message', 'store')->name('message.store');
+       Route::post('message/{message}', 'destroy')->name('message.destroy');
+   });
     Route::get('/', [HomeController::class, 'home'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
